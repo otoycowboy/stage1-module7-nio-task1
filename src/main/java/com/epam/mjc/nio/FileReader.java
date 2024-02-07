46c299cae4e85438d9c08d045af5d0fa255f04b4
otoycowboy <dennissmart777@gmail.com>
2024-02-07 18:55:59 +0100

    Task completed and tested

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,43 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try (RandomAccessFile aFile = new RandomAccessFile(file, "r");
+             FileChannel readChannel = aFile.getChannel()) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (readChannel.read(buffer) > 0) {
+                buffer.flip();
+                profile.setName(getClearInfo(buffer));
+                profile.setAge(Integer.valueOf(getClearInfo(buffer)));
+                profile.setEmail(getClearInfo(buffer));
+                profile.setPhone(Long.valueOf(getClearInfo(buffer)));
+                buffer.clear();
+            }
+
+        } catch (IOException e) {
+            return null;
+        }
+
+        return profile;
+    }
+
+    private String getClearInfo(ByteBuffer readBuffer) throws IOException {
+        StringBuilder info = new StringBuilder();
+        int c;
+        while ((c = readBuffer.get()) != '\n') {
+            info.append((char) c);
+        }
+
+        info = new StringBuilder(info.substring(info.toString().lastIndexOf(':') + 1).trim());
+        return info.toString();
     }
 }
